382035fd38e9d9c075c866a8e5c90541a4bfca4a
Dimitri Felix Cerqueira <dimitrifelix1314@gmail.com>
2022-02-02 18:20:40 -0300

    Pessimo commit adicionando muitas coisas

---
+++ Exercicios_Trybe/Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-primeiros-passos/part2.js
@@ -0,0 +1,45 @@
+let movie = "Avengers"; //string literal
+let score = 10.89; //number literal
+let isValid = true; //boolean
+let name; // undefined
+let color = null; //redefinição
+
+//typeof
+
+//+ ,-, *, /, **
+
+let salary = 3500
+
+// console.log(salary + salary);
+// console.log(salary - salary);
+// console.log(salary * salary);
+// console.log(salary / salary);
+// console.log(3 ** 3);
+
+// salary++;
+// salary--;
+
+// console.log(salary);
+
+//practice
+
+// let patientId = 50;
+// pacientId = '50';
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(pacientId)
+
+//practiceTwo
+
+const base = 5;
+let height = 8;
+const area = base*height;
+const perimeter = base+base+height+height;
+
+console.log(base*height);
+console.log(perimeter);
