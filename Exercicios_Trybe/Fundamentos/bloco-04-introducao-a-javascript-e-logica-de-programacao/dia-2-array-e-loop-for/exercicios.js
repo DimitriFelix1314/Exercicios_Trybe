b16e2058483a2911722f5c0c0c53801af40f5204
Dimitri Felix Cerqueira <dimitrifelix1314@gmail.com>
2022-02-03 17:02:06 -0300

    adicionando exercicio 3

--- Exercicios_Trybe/Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios.js
+++ Exercicios_Trybe/Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios.js
@@ -8,17 +8,24 @@
 
 //exercicios 2
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum = 0;
 
-for (let i=0; i<numbers.length; i++) {
-    sum += numbers[i];
-}
-console.log(sum);
+// for (let i=0; i<numbers.length; i+=1) {
+//     sum += numbers[i];
+// }
+// console.log(sum);
 
 //exercicios 3
 
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+for (let i=0; i<numbers.length; i+=1){
+    sum += numbers[i];
+}
+console.log(sum/numbers.length);
+
 
 // //exercicios 4
 
