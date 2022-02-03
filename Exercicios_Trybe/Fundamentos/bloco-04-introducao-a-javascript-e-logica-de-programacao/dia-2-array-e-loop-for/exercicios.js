1ba48a658faf9890a2a1319de23388e34093f6a3
Dimitri Felix Cerqueira <dimitrifelix1314@gmail.com>
2022-02-03 16:39:51 -0300

    adicionando primeiro exercicio

--- Exercicios_Trybe/Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios.js
+++ Exercicios_Trybe/Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios.js
@@ -1,5 +1,11 @@
 //exercicios 1
 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (index = 0; index < numbers.length; index +=1) {
+    console.log(numbers[index]);
+}
+
 //exercicios 2
 
 //exercicios 3
