808cf82cb3b26035328c4e44d285e00bb55a4a06
Dimitri Felix Cerqueira <dimitrifelix1314@gmail.com>
2022-02-03 18:04:32 -0300

    exercicios.js

--- Exercicios_Trybe/Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios.js
+++ Exercicios_Trybe/Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios.js
@@ -46,20 +46,33 @@
 
 // //exercicios 5
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let higherNumber = numbers[0];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let higherNumber = numbers[0];
 
-for (let index = 1; index < numbers.length; index += 1) {
-  if (numbers[index] > higherNumber) {
-    higherNumber = numbers[index];
-  }
-}
+// for (let index = 1; index < numbers.length; index += 1) {
+//   if (numbers[index] > higherNumber) {
+//     higherNumber = numbers[index];
+//   }
+// }
 
-console.log(higherNumber);
+// console.log(higherNumber);
 
 // //exercicios 6
 
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 !== 0) {
+    result += 1;
+  }
+}
+
+if (result === 0) {
+  console.log('nenhum valor ímpar encontrado');
+} else {
+  console.log(result);
+}
 
 // //exercicios 7
 
@@ -72,6 +85,8 @@ console.log(higherNumber);
 // //exercicios 9
 
 // let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//Exercicios bonus
 
 // //exercicios 10
 
