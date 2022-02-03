0e93a2ac0da2f41409c6eef63d16729f17ec3949
Dimitri Felix Cerqueira <dimitrifelix1314@gmail.com>
2022-02-03 18:08:19 -0300

    adiciona exercicio 7

--- Exercicios_Trybe/Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios.js
+++ Exercicios_Trybe/Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios.js
@@ -59,24 +59,33 @@
 
 // //exercicios 6
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let result = 0;
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let result = 0;
 
-for (let index = 0; index < numbers.length; index += 1) {
-  if (numbers[index] % 2 !== 0) {
-    result += 1;
-  }
-}
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] % 2 !== 0) {
+//     result += 1;
+//   }
+// }
 
-if (result === 0) {
-  console.log('nenhum valor ímpar encontrado');
-} else {
-  console.log(result);
-}
+// if (result === 0) {
+//   console.log('nenhum valor ímpar encontrado');
+// } else {
+//   console.log(result);
+// }
 
 // //exercicios 7
 
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let lowerNumber = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  if (numbers[index] < lowerNumber) {
+    lowerNumber = numbers[index];
+  }
+}
+
+console.log(lowerNumber);
 
 // //exercicios 8
 
