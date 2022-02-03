42c88c7def2fc9ea156d479083edd09e529f0b3e
Dimitri Felix Cerqueira <dimitrifelix1314@gmail.com>
2022-02-03 17:15:14 -0300

    adicionando exercicio 5(dificil esse em...)

--- Exercicios_Trybe/Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios.js
+++ Exercicios_Trybe/Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios.js
@@ -29,24 +29,33 @@
 
 // //exercicios 4
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum = 0;
 
-for (let i=0; i<numbers.length; i+=1){
-    sum += numbers[i];
-}
-let average=sum/numbers.length;
-console.log(average);
+// for (let i=0; i<numbers.length; i+=1){
+//     sum += numbers[i];
+// }
+// let average=sum/numbers.length;
+// console.log(average);
 
-if (average>20){
-    console.log("valor maior que vinte");
-} else {
-    console.log("valor menor ou igual a vinte;")
-}
+// if (average>20){
+//     console.log("valor maior que vinte");
+// } else {
+//     console.log("valor menor ou igual a vinte;")
+// }
 
 // //exercicios 5
 
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let higherNumber = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  if (numbers[index] > higherNumber) {
+    higherNumber = numbers[index];
+  }
+}
+
+console.log(higherNumber);
 
 // //exercicios 6
 
