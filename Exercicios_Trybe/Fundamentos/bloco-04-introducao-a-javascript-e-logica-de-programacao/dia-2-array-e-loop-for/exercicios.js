f3e27fcd3c0537250962e02fd91f119900169350
Dimitri Felix Cerqueira <dimitrifelix1314@gmail.com>
2022-02-03 16:34:08 -0300

    adicionando uma organizaçao para os exercicios

---
+++ Exercicios_Trybe/Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios.js
@@ -0,0 +1,21 @@
+//exercicios 1
+
+//exercicios 2
+
+//exercicios 3
+
+//exercicios 4
+
+//exercicios 5
+
+//exercicios 6
+
+//exercicios 7
+
+//exercicios 8
+
+//exercicios 9
+
+//exercicios 10
+
+//exercicios 11
