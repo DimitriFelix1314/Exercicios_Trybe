75c9330843e1d80076810a8e1b47d0487ba21f0c
Dimitri Felix Cerqueira <dimitrifelix1314@gmail.com>
2022-02-03 18:16:52 -0300

    adicionando exercicio 8

--- Exercicios_Trybe/Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios.js
+++ Exercicios_Trybe/Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios.js
@@ -76,20 +76,36 @@
 
 // //exercicios 7
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let lowerNumber = numbers[0];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let lowerNumber = numbers[0];
 
-for (let index = 1; index < numbers.length; index += 1) {
-  if (numbers[index] < lowerNumber) {
-    lowerNumber = numbers[index];
-  }
-}
+// for (let index = 1; index < numbers.length; index += 1) {
+//   if (numbers[index] < lowerNumber) {
+//     lowerNumber = numbers[index];
+//   }
+// }
 
-console.log(lowerNumber);
+// console.log(lowerNumber);
 
 // //exercicios 8
 
 // let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultadoDaImpressao = 0;
+
+// for(let i=0; i < numbers.length; i+=1) {
+//     let resultadoDaImpressao = numbers[i]>=1 && numbers[i] <=25;
+//     console.log(resultadoDaImpressao);
+// }
+
+//another way
+
+let numbers = [];
+
+for (let i=1; i<=25; i+=1) {
+    numbers.push(i);
+}
+
+console.log(numbers)
 
 // //exercicios 9
 
