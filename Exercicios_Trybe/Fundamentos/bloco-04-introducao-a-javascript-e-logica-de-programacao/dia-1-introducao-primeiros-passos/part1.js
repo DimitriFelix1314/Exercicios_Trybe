404f26fbd46f5ed029d58a4e41dce5e5c87f70f5
Dimitri Felix Cerqueira <dimitrifelix1314@gmail.com>
2022-03-04 15:57:51 -0300

    Adicionando README e Dia 4.1

--- Exercicios_Trybe/Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-primeiros-passos/part1.js
+++
@@ -1,9 +0,0 @@
-const myName = "Dimitri";
-const birthCity = "Mogi das Cruzes";
-birthCity = "vaiDarErro"
-let birthYear = 2000;
-birthYear = 2030;
-
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
---
+++ Exercicios_Trybe/Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-primeiros-passos/script.js
@@ -0,0 +1,49 @@
+// // Exércicio 1
+// const a = 10;
+// const b = 5;
+
+// console.log('Soma: ' + (a + b));
+// console.log('Subtração: ' + (a - b));
+// console.log('Multiplicação: ' + (a * b));
+// console.log('Divisão: ' + (a / b));
+// console.log('Módulo: ' + (a % b));
+
+// // Exércicio 2
+// const a = 20;
+// const b = 21;
+
+// if (a > b) {
+//   console.log("'a' é maior que 'b'");
+// } else {
+//   console.log("'b' é maior que 'a'");
+// };
+
+// Exércicio 3
+
+// const a = 36;
+// const b = 25;
+// const c = 47;
+
+// if (a > b && a > c) {
+//   console.log(a + " é maior do que " + b + " e " + c);
+// } else if (b > a && b > c) {
+//   console.log(b + " é maior do que " + b + " e " + c);
+// } else if (c > a && c > b) {
+//   console.log(c + " é maior do que " + b + " e " + a);
+// } else {
+//   console.log("Todos os numeros são iguais");
+// }
+
+// Exercicio 4
+
+// const value = 0;
+
+// if (value > 0) {
+//   console.log("Positive");
+// } else if (value < 0) {
+//   console.log("Negative");
+// } else {
+//   console.log("Zero");
+// }
+
+// Exercicio 5
