e2365147fd414d59f21b6211a92d64571be002c0
Dimitri Felix Cerqueira <dimitrifelix1314@gmail.com>
2022-03-04 16:27:07 -0300

    Exercicio 6 dia 4.1

--- Exercicios_Trybe/Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-primeiros-passos/script.js
+++ Exercicios_Trybe/Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-primeiros-passos/script.js
@@ -47,3 +47,43 @@
 // }
 
 // Exercicio 5
+
+// const a = 60;
+// const b = 60;
+// const c = 60;
+
+// if (a < 0 || b < 0 || c < 0) {
+//   console.log("Inválido")
+// } else if (a + b + c === 180) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// Exercicio 6
+
+const chessPiece = 'Bishop';
+
+switch (chessPiece.toLowerCase()) {
+  case 'king':
+    console.log('Rei-> All direction with one step.');
+    break;
+  case 'bishoo':
+    console.log('Bispo-> Diagonal.');
+    break;
+  case 'queen':
+    console.log('Rainha-> All directions.');
+    break;
+  case 'knight':
+    console.log('Cavalo -> "L" in shape.');
+    break;
+  case 'rook':
+    console.log('Torre -> Horizontally e Vertically.');
+    break;
+  case 'pawn':
+    console.log("Peão -> Just forward, at the first moviment it may does 2 steps.");
+    break;
+  default:
+    console.log('Error, there is no ' + chessPiece + '!');
+    break;
+};
